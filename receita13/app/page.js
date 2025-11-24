a94f9db92afb1517b136a86fb9efcec278fd6533
LuizFelixDev <luizhenriquefelix138@gmail.com>
2025-11-24 10:45:53 -0300

    Receita 13 concluída

---
+++ receita13/app/page.js
@@ -0,0 +1,12 @@
+export default function Home(){
+    return (
+        <div>
+            <div>Menu principal</div>
+            <div>
+                <h1>
+                    Flameno é campeão!
+                </h1>
+            </div>
+        </div>      
+    )
+}
