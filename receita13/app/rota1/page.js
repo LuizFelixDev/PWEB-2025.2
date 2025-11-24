4f94dd2fefa0d8bb78fb85343893feaee094d7ab
LuizFelixDev <luizhenriquefelix138@gmail.com>
2025-11-24 11:02:41 -0300

    Exercício 1 concluído

---
+++ receita13/app/rota1/page.js
@@ -0,0 +1,12 @@
+export default function compras(){
+    return (
+        <div>
+            <div>Menu de compras</div>
+            <div>
+                <h1>
+                    Compra relaizado com sucesso!
+                </h1>
+            </div>
+        </div>      
+    )
+}
